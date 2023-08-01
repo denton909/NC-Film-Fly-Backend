a7a90a96839eb509a16b78cb6da0814d7ff00a24
Philip Denton <Phil@philips-mbp.home>
2023-08-01 11:23:59 +0100

    updated seed

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,12 @@
 const format = require('pg-format')
 const db = require('./connection')
-const csvPath = String(__dirname)+'/csv_files/movies.csv'
+// const csvPath = String(__dirname)+'/csv_files/movies.csv'
+const movies = require('./csv_files/movies.json')
+const crew = require('./csv_files/crew.json')
 const users = require('./user_data/test_data')
-//yo
 
-console.log(csvPath)
+
+
 
 const seed = () => {
     return db.query('DROP TABLE IF EXISTS movies')
@@ -21,7 +23,7 @@ const seed = () => {
         return db.query('DROP TABLE IF EXISTS directors')
     })
     .then(()=> {
-        return db.query('CREATE TABLE movies (budget INT, genres TEXT, homepage TEXT, id INT, keywords TEXT, original_language TEXT, original_title TEXT, overview TEXT, popularity TEXT, production_companies TEXT, production_countries TEXT, release_date DATE, revenue BIGINT, runtime TEXT, spoken_languages TEXT, status TEXT, tagline TEXT, title TEXT, vote_average TEXT, vote_count INT)')
+        return db.query('CREATE TABLE movies (budget INT, genres TEXT, homepage TEXT, id INT, keywords TEXT, original_language TEXT, original_title TEXT, overview TEXT, popularity TEXT, production_companies TEXT, production_countries TEXT, release_date TEXT, revenue BIGINT, runtime TEXT, spoken_languages TEXT, status TEXT, tagline TEXT, title TEXT, vote_average TEXT, vote_count INT)')
     })
     .then(()=>{
         return db.query('CREATE TABLE users (user_id SERIAL PRIMARY KEY, name VARCHAR(30), username VARCHAR(30) NOT NULL, email_address VARCHAR(50) NOT NULL, password VARCHAR(30) NOT NULL, genre_scores TEXT, genre_pref TEXT, actor_pref TEXT, actor_scores TEXT, director_pref TEXT, director_scores TEXT, liked_movies TEXT, disliked_movies TEXT, watched_recently TEXT)')
@@ -36,7 +38,12 @@ const seed = () => {
         return db.query('CREATE TABLE actors (actor_id SERIAL PRIMARY KEY, name TEXT, number_of_films INT)')
     })
     .then(()=> {
-        return db.query(`COPY movies (budget, genres, homepage, id, keywords, original_language, original_title, overview, popularity, production_companies, production_countries, release_date, revenue, runtime, spoken_languages, status, tagline, title, vote_average, vote_count) FROM '/Users/Phil/Desktop/NorthCoders/CourseWork/BE-Film-Fly/db/csv_files/movies.csv' DELIMITER ',' CSV HEADER;`)
+        const query = format(`INSERT INTO movies (budget, genres, homepage, id, keywords, original_language, original_title, overview, popularity, production_companies, production_countries, release_date, revenue, runtime, spoken_languages, status, tagline, title, vote_average, vote_count) VALUES %L; `, movies.map((movie) => {
+
+            return [movie.budget, movie.genres, movie.homepage, movie.id, movie.keywords, movie.original_language, movie.original_title, movie.overview, movie.popularity, movie.production_companies, movie.production_countries, movie.release_date, movie.revenue, movie.runtime, movie.spoken_languages, movie.status, movie.tagline, movie.title, movie.vote_average, movie.vote_count]
+        }))
+        return db.query(query)
+            
     })
     .then(()=> {
         const query =  format(`INSERT INTO users (name, username, email_address, password, genre_scores, genre_pref, actor_pref, actor_scores, director_pref, director_scores, liked_movies, disliked_movies, watched_recently) VALUES %L;`, users().map((user) => {
@@ -47,10 +54,17 @@ const seed = () => {
         return db.query(query)
     })
     .then(()=> {
-        return db.query(`COPY crew (movie_id, title, "cast", crew) FROM '/Users/Phil/Desktop/NorthCoders/CourseWork/BE-Film-Fly/db/csv_files/crew.csv' DELIMITER ',' CSV HEADER;`)
+        const query = format(`INSERT INTO crew (movie_id, title, "cast", crew) VALUES %L;`, crew.map((obj)=> {
+            return [obj.movie_id, obj.title, obj.cast, obj.crew]
+        }))
+
+        return db.query(query)
+        
     })
+
 
 
 }
 
 module.exports = seed
+
